c58843d3c1137126bcd991b0272809be36261887
ryotaro-tenya0727 <ryotaro123110@gmail.com>
2022-08-02 19:05:13 +0900

    ADD: グーグルログイン機能実装

--- app/controllers/google_login_api_controller.rb
+++ app/controllers/google_login_api_controller.rb
@@ -1,7 +1,10 @@
 class GoogleLoginApiController < ApplicationController
+  protect_from_forgery except: :callback
+
   def callback
     key_source = Google::Auth::IDTokens::JwkHttpKeySource.new("https://www.googleapis.com/oauth2/v3/certs")
-    Google::Auth::IDTokens::Verifier.new(key_source: key_source).verify(params[:credential], aud: ENV['GOOGLEAUTH_ID'])
-    redirect_back_or_to root_path
+    user_data = Google::Auth::IDTokens::Verifier.new(key_source: key_source).verify(params[:credential], aud: ENV['GOOGLEAUTH_ID'])
+    p user_data["sub"]
+    redirect_to root_path
   end
 end
